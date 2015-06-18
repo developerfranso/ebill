28633feae65493e2d17efc737c8a2660a185880b
developerfranso <developerfranso@users.noreply.github.com>
2015-06-18 13:18:03 +0300

    Update ebill.sql

--- Database/ebill.sql
+++ Database/ebill.sql
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 --
 -- Database: `ebill`
 --
-DROP DATABASE `ebill`;
+
 CREATE DATABASE IF NOT EXISTS `ebill` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `ebill`;
 
